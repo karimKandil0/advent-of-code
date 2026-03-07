0920d83c7788fb96c8ab3b6cd88a780a42ba2f97
karimKandil0 <kimo989yt@gmail.com>
2026-03-08 01:32:34 +0200

    Solved AoC 2024 Day 1. Python and Rust

--- solutions/rust/day01/src/main.rs
+++ solutions/rust/day01/src/main.rs
@@ -1,11 +1,47 @@
+use std::colletion:HashMap;
 use std::fs;
 
+fn parse(input: &str) -> i64 {
+  let left = Vec::new();
+  let right = Vec::new();
+
+  for line in input.lines() {
+      let parts: Vec<&str> = line.split(": ").collect();
+      let a = parts[0].parse::<i32>().expect("Failed to parse");
+      let b = parts[1].parse::<i32>().expect("Failed to parse");
+      left.push(a);
+      right.push(b);
+  }
+
+  (left, right)
+
+}
+
 fn part1(input: &str) -> i64 {
-    0
+    let (mut left, mut right) = parse(input);
+
+    left.sort();
+    right.sort();
+
+    left.iter()
+        .zip(right.iter())
+        .map(|(a, b)| (a - b).abs())
+        .sum()
 }
 
 fn part2(input: &str) -> i64 {
-    0
+    let  (left, right) = parse(input);
+
+    let mut counts = HashMap::new();
+    for &num in &right {
+        *counts.entry(num).or_insert(0) += 1;
+    }
+
+    left.iter()
+        .map(|x| x * counts.get(x).unwrap_or(&0))
+        .sum()
+
+
 }
 
 fn main() {
